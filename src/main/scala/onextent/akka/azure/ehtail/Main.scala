bd290b6d2324e4daafd0a48ae350e0fe90efe390
Ed Sweeney <ed@onextent.com>
2018-09-09 09:40:48 -0700

    init

--- src/main/scala/onextent/akka/azure/ehtail/Main.scala
+++ src/main/scala/onextent/akka/azure/ehtail/Main.scala
@@ -1,5 +1,7 @@
 package onextent.akka.azure.ehtail
 
+import org.json4s._
+import org.json4s.native.JsonMethods._
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.{Done, NotUsed}
 import com.typesafe.config.{Config, ConfigFactory}
@@ -10,16 +12,11 @@ import onextent.akka.eventhubs.Eventhubs._
 
 import scala.concurrent.Future
 
-object MultiPartitionExample {
+object Run {
 
   def apply(): Unit = {
 
-    val consumer: Sink[(String, AckableOffset), Future[Done]] =
-      Sink.foreach(m => {
-        //println(s"SUPER SOURCE: ${m._1.substring(0, 160)}")
-        //println(s"SINGLE SOURCE: ${m._1}")
-        m._2.ack()
-      })
+    val consumer: Sink[(String, AckableOffset), Future[Done]] = Sink.foreach(_._2.ack())
 
     val toConsumer = createToConsumer(consumer)
 
@@ -31,57 +28,24 @@ object MultiPartitionExample {
         createPartitionSource(pid, cfg)
 
       val flow = Flow[(String, AckableOffset)].map((x: (String, AckableOffset)) => {
-        //println(s"do something! pid: $pid ${x._1.substring(0, 9)}")
         if (conf.getBoolean("main.pretty") && x._1.charAt(0) == '{') {
-          import org.json4s._
-          import org.json4s.native.JsonMethods._
           val parsedJson: JValue = parse(x._1)
           println(s"consumer pid $pid received:\n${pretty(render(parsedJson))}")
         } else {
           println(s"consumer pid $pid received:\n${x._1}")
         }
-
         x
       })
 
-//      val xform = Flow[(String, AckableOffset)].map((x: (String, AckableOffset)) => {
-//
-//        import onextent.data.navipath.dsl.NaviPathSyntax._
-//        val txnId = x._1.query[String]("$.someId")
-//        println(s"do something with txnid! $txnId")
-//        (txnId.getOrElse("unknown"), x._2)
-//      })
-
       src.via(flow).runWith(toConsumer)
-      //src.via(xform).runWith(toConsumer)
 
     }
   }
 
 }
 
-object SinglePartitionExample {
-
-  def apply(): Unit = {
-
-    val cfg: Config = ConfigFactory.load().getConfig("eventhubs-1")
-
-    val source1 = createPartitionSource(0, cfg)
-
-    source1.runForeach(m => {
-      println(s"SINGLE SOURCE: ${m._1.substring(0, 160)}")
-      m._2.ack()
-    })
-
-  }
-
-}
-
 object Main extends App {
 
-  // TODO: create a toFlow example
-
-  MultiPartitionExample()
-  //SinglePartitionExample()
+  Run()
 
 }
